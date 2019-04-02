e28dd9af0e1f31018669588cfd6a2a073434295b
Bob Brown <robertbernardbrown@gmail.com>
2019-04-01 22:08:16 -0400

    playing around a little more with requesting bitcoin data

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-const http = require('http');
 const request = require('request');
+const express = require('express');
+const app = express();
+let btcPrice;
+let btcBlock;
 
-http.createServer(function (req, res) {
-    request({
-        url: 'https://blockchain.info/stats?format=json',
-        json: true
-    }, function (err, response, body) {
-        if (err) {
-            throw new Error(err);
-        }
-        console.log(body)
-    })
-    res.end('bitcoin to the moon');
-}).listen(8080); 
+request({
+    url: 'https://blockchain.info/stats?format=json',
+    json: true
+}, function(err, response, body) {
+    btcPrice = body.market_price_usd;
+    btcBlock = body.n_blocks_total;
+})
+
+app.get('/', function(req, res) {
+    res.send('Current block price: ' + btcPrice);
+});
+
+app.get('/block', function(req, res) {
+    res.sendFile('index.html');
+});
+
+app.listen(8088, function() {
+    console.log('go');
+})
