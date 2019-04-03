7fd44c9bab1750a5b97aa2a1d69daf07228cb392
Bob Brown <robertbernardbrown@gmail.com>
2019-04-02 21:39:39 -0400

    playing around with request and returning json information to send in res

--- app.js
+++ app.js
@@ -5,26 +5,38 @@ const bodyParser = require('body-parser');
 const bitCore = require('bitcore-lib');
 
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true
 }));
 
 app.use(bodyParser.json());
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/wallet', function(req, res) {
-    const brainsrc = req.body.brainsrc;
-    console.log(brainsrc);
-    const input = new Buffer.from(brainsrc);
-    const hash = bitCore.crypto.Hash.sha256(input);
-    const bn = bitCore.crypto.BN.fromBuffer(hash);
-    const pk = new bitCore.PrivateKey(bn).toWIF();
-    const addy = new bitCore.PrivateKey(bn).toAddress();
-    res.send(`The Brain Wallet of: ${brainsrc}<br> Addy: ${addy}<br> Private Key: ${pk}`);
+app.post('/wallet', function (req, res) {
+  const brainsrc = req.body.brainsrc;
+  console.log(brainsrc);
+  const input = new Buffer.from(brainsrc);
+  const hash = bitCore.crypto.Hash.sha256(input);
+  const bn = bitCore.crypto.BN.fromBuffer(hash);
+  const pk = new bitCore.PrivateKey(bn).toWIF();
+  const addy = new bitCore.PrivateKey(bn).toAddress();
+  let blockchainBody;
+  request({
+    url: `https://blockchain.info/address/${addy}?format=json`,
+    json: true,
+  }, function (err, response, body) {
+    if (err) {
+      console.log(err)
+    }
+    blockchainBody = body;
+    console.log(blockchainBody);
+    // res.send(`${blockchainBody}`);
+    res.send(`The Brain Wallet of: ${brainsrc}<br> Addy: ${blockchainBody.address}<br> Private Key: ${pk}`);
+  });
 });
 
-app.listen(8088, function() {
-    console.log('go');
+app.listen(8088, function () {
+  console.log('go');
 })
